5095115789d47c83bf0317dd6fe6c78108b59c6b
Ahmed-Ehab-601 <medo.losy@gmail.com>
2025-03-07 00:47:59 +0200

    handle and node

---
+++ frontend/signal-flow-graph/src/Flow.jsx
@@ -0,0 +1,278 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import ReactFlow, {
+  addEdge,
+  useEdgesState,
+  useNodesState,
+  Background,
+  Controls,
+  useReactFlow,
+  ReactFlowProvider,
+} from "reactflow";
+import "reactflow/dist/style.css";
+
+import Node from "./Node";
+import queueicon from "./assets/queue.svg";
+import counter from "./assets/counter.svg";
+import redoicon from "./assets/redo.svg";
+import newsim from "./assets/play.svg";
+import machineicon from "./assets/machine.svg";
+import deleteicon from "./assets/trash (1).svg";
+import HandleSimulate from "./HandleSimulate";
+import stopicon from './assets/stop.svg';
+import Replay from "./Replay";
+import Simulate from "./StopSimulate";
+
+const nodeTypes = { node: Node };
+
+const SimulationFlow = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+
+  const [NodeID, setNodeID] = useState(1);
+
+  const [queuemenu, setqueuemenu] = useState(false); // Initialize shape state inside the component
+  const [menu, setMenu] = useState(false); // Initialize as boolean
+  const [numberOfProducts, setNumberOfProducts] = useState(0); // Correct state name
+
+  const deleteall = () => {
+    setNodes([]);
+    setEdges([]);
+    setNodeID(1); 
+    Simulate();
+  };
+
+  const handleNumberOfProductsChange = (e) => {
+    setNumberOfProducts(e.target.value);
+  };
+
+  const onConnect = (params) => {
+    const { source, target } = params;
+
+    const sourceNode = nodes.find((node) => node.id === source);
+    const targetNode = nodes.find((node) => node.id === target);
+
+    if (!sourceNode || !targetNode) {
+      console.error("Source or target node not found.");
+      return;
+    }
+    const animatedEdge = {
+      ...params,
+      style: {
+        stroke: "#808080",
+        strokeWidth: 2,
+        strokeDasharray: "10 5", // Dashed pattern
+        strokeDashoffset: 0,    // Initial offset
+        animation: "dash-move 5s linear infinite", // Apply animation
+      },
+      markerEnd: { type: "arrowclosed", color: "#808080" },
+    };
+      setEdges((eds) => addEdge(animatedEdge, eds));
+    
+  };
+  // State for floating node
+  const [floatingNode, setFloatingNode] = useState(null);
+  const { screenToFlowPosition } = useReactFlow();
+
+  // Mouse move handler
+  const handleMouseMove = (event) => {
+    if (floatingNode) {
+      const canvasPosition = screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+      setFloatingNode({
+        ...floatingNode,
+        position: canvasPosition,
+      });
+    }
+  };
+
+  // Mouse click handler
+  const handleMouseClick = () => {
+    if (floatingNode) {
+      setNodes((nodes) => [...nodes, floatingNode]);
+      setFloatingNode(null);
+    }
+  };
+
+
+  // creating queue node
+  const handleCreateNode = (event, type) => {
+    const canvasPosition = screenToFlowPosition({
+      x: event.clientX,
+      y: event.clientY,
+    });
+    let node = null;
+    if (type === "start") {
+      node = {
+        id: `Input`,
+        position: canvasPosition,
+        data: { label: `Input` },
+        type: "node",
+      };
+      const node = nodes.find((node)=> node.id === node.id)
+      if(node){
+        alert(`Cannot have multible start`);
+        return
+      }
+      else{
+        setFloatingNode(node)
+      }
+    } else if (type === "end") {
+      node = {
+        id: `Output`,
+        position: canvasPosition,
+        data: { label: `Output` },
+        type: "node",
+      };
+      const node = nodes.find((node)=> node.id === node.id)
+      if(node){
+        alert(`Cannot have multible end`);
+        return
+      }
+      else{
+        setFloatingNode(node)
+      }
+    } else {
+       node = {
+        id: `v${NodeID}`,
+        position: canvasPosition,
+        data: { label: `v${NodeID}`},
+        type: "node",
+      };
+      setFloatingNode(node);
+      setNodeID(NodeID + 1);
+    }
+    
+  };
+  return (
+    <div
+      className="flow"
+      onMouseMove={handleMouseMove}
+      onClick={handleMouseClick}
+    >
+      <div className="bar">
+        
+        <button
+          className="icon"
+          onClick={() => {
+            setqueuemenu(!queuemenu);
+            setMenu(false);
+          }}
+        >
+          <img src={queueicon} alt="queue" />
+        </button>
+        {queuemenu && (
+          <menu className="menudisplay">
+            <button
+              className="settype"
+              onClick={(event) => {
+                setqueuemenu(false);
+                handleCreateNode(event, "start");
+              }}
+            >
+              Start
+            </button>
+            <button
+              className="settype"
+              onClick={(event) => {
+                setqueuemenu(false);
+                handleCreateNode(event);
+              }}
+            >
+              Normal
+            </button>
+            <button
+              className="settype"
+              onClick={(event) => {
+                setqueuemenu(false);
+                handleCreateNode(event, "end");
+              }}
+            >
+              End
+            </button>
+          </menu>
+        )}
+
+        <button
+          className="icon"
+          onClick={() => {
+            setMenu(!menu);
+            setqueuemenu(false);
+          }}
+        >
+          <img src={counter} alt="counter" />
+        </button>
+        {/* Conditional rendering of the menu */}
+        {menu && (
+          <div className="inputmenu">
+            <input
+              id="products"
+              type="number" // Changed to number input for product count
+              value={numberOfProducts}
+              onChange={handleNumberOfProductsChange}
+              style={{
+                background: "white",
+                opacity: 0.7, // Set opacity to make input visible
+                position: "absolute",
+                width: "50%",
+                zIndex: "10px",
+                padding: "10px",
+                pointerEvents: "auto",
+                color: "black",
+                border: "black",
+              }}
+            />
+          </div>
+        )}
+        <button
+          className="icon"
+          onClick={() => {
+            setMenu(false);
+            setqueuemenu(false);
+            HandleSimulate(nodes, edges, numberOfProducts,setNodes);
+          }}
+        >
+          <img src={newsim} alt="new" />
+        </button>
+        <button
+          className="icon"
+          onClick={() => {
+            setMenu(false);
+            setqueuemenu(false);
+            Replay(setNodes);
+          }}
+        >
+          <img src={redoicon} alt="redo" />
+        </button>
+        <button className="icon"onClick={Simulate}>
+        <img src={stopicon} alt="stop" />
+        </button>
+        <button className="icon" onClick={deleteall}>
+          <img src={deleteicon} alt="delete" />
+        </button>
+      </div>
+      <ReactFlow
+        nodes={floatingNode ? [...nodes, floatingNode] : nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeTypes}
+        fitView
+      >
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
+};
+
+const WrappedSimulationFlow = () => (
+  <ReactFlowProvider>
+    <SimulationFlow />
+  </ReactFlowProvider>
+);
+
+export default WrappedSimulationFlow;
---
+++ frontend/signal-flow-graph/src/Node.jsx
@@ -0,0 +1,17 @@
+import { Handle } from "reactflow";
+function Node({data}) {
+    return (
+        <div className="node">
+        <p>{data.label}</p>
+       {data.label!="Input"&& <Handle type="target" position="left" id="left" className="handle" />}
+       {data.label!="Input"&& <Handle type="source" position="left" id="left" className="handle" />}
+        {data.label!="Output"&&  <Handle type="source" position="right" id="right" className="handle" />}
+        {data.label!="Output"&&  <Handle type="target" position="right" id="right" className="handle" />}
+        {data.label!="Input" && data.label!="Output" && <Handle type="target" position="top" id="top" className="handle" />}
+        {data.label!="Input" && data.label!="Output" && <Handle type="source" position="top" id="top" className="handle" />}
+        {data.label!="Input" && data.label!="Output" && <Handle type="target" position="bottom" id="bottom" className="handle" />}
+        {data.label!="Input" && data.label!="Output" && <Handle type="source" position="bottom" id="bottom" className="handle" />} </div>
+
+    );
+}
+export default Node;
